0ef95ebcc826c8fe8b107f6fbf2ce44f0cf2f4d7
GoudaKaas <fabricegouba270@gmail.com>
2025-05-01 22:29:31 +0200

    Javascript timer gemaakt die ieder svg op zijn eigen toont a.d.h.v intervals.

--- animate.js
+++ animate.js
@@ -1,15 +1,31 @@
-/* State machine of animation */
-const AnimationStateCount = {
-	Intro: true,
-	Sapflow: false,
-	Sensors: false,
-	Heating: false
-} 
+window.addEventListener("load", (e) => {
+	console.log("page is fully loaded");
+	/* Text svg1 show after 1s */
+	document.getElementById("one").classList.add("enabled");
+	console.log("page passed by timer");
+	setTimeout(transition1, 5000);
+  });
 
- 
-/* 
-
-1) Make a global div that contain all the SVG elements
-2) AFTER animation is done make the confirm buttons trabnition to the next SVG
-3) Begin the next animation c-:
-*/
+function transition1() {
+	console.log("page passed by function 1");
+	document.getElementById("one").classList.remove("enabled");
+	document.getElementById("one").classList.add("disabled");
+	document.getElementById("two").classList.remove("disabled");
+	document.getElementById("two").classList.add("enabled");
+	setTimeout(transition2, 5000);
+}
+function transition2() {
+	console.log("page passed by function 2");
+	document.getElementById("two").classList.remove("enabled");
+	document.getElementById("two").classList.add("disabled");
+	document.getElementById("three").classList.remove("disabled");
+	document.getElementById("three").classList.add("enabled");
+	setTimeout(transition3, 5000);
+}
+function transition3() {
+	console.log("page passed by function 3");
+	document.getElementById("three").classList.remove("enabled");
+	document.getElementById("three").classList.add("disabled");
+	document.getElementById("four").classList.remove("disabled");
+	document.getElementById("four").classList.add("enabled");
+}
