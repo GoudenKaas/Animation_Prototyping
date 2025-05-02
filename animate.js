9f49e24d4d3bcf2bd9e03a23e34a7723555b3601
GoudaKaas <fabricegouba270@gmail.com>
2025-05-02 15:58:16 +0200

    Aanpassing: 1) Fade-in animaties werken voor alle svg's op verschijning. 2) De animaties die je op de elementen toggled zijn gepaard met de animatie die je erop wilt. Verander deze tijden en animeer alle zaken binnen de tijdslimieten.

--- animate.js
+++ animate.js
@@ -2,16 +2,17 @@ window.addEventListener("load", (e) => {
 	console.log("page is fully loaded");
 	/* Text svg1 show after 1s */
 	document.getElementById("one").classList.add("enabled");
+	document.getElementById("one").classList.add("one");
 	console.log("page passed by timer");
 	setTimeout(transition1, 5000);
   });
 
 function transition1() {
 	console.log("page passed by function 1");
-	document.getElementById("one").classList.remove("enabled");
 	document.getElementById("one").classList.add("disabled");
 	document.getElementById("two").classList.remove("disabled");
 	document.getElementById("two").classList.add("enabled");
+	document.getElementById("two").classList.add("two");
 	setTimeout(transition2, 5000);
 }
 function transition2() {
@@ -20,6 +21,7 @@ function transition2() {
 	document.getElementById("two").classList.add("disabled");
 	document.getElementById("three").classList.remove("disabled");
 	document.getElementById("three").classList.add("enabled");
+	document.getElementById("three").classList.add("three");
 	setTimeout(transition3, 5000);
 }
 function transition3() {
@@ -28,4 +30,5 @@ function transition3() {
 	document.getElementById("three").classList.add("disabled");
 	document.getElementById("four").classList.remove("disabled");
 	document.getElementById("four").classList.add("enabled");
+	document.getElementById("four").classList.add("four");
 }
