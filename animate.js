45088f10bcbeea36c0f8cdc82c90a2a5f4be9c31
GoudaKaas <fabricegouba270@gmail.com>
2025-05-03 20:22:56 +0200

    commit om huidige versie te saven (doel nog niet bereikt).

--- animate.js
+++ animate.js
@@ -1,46 +1,52 @@
 const startBtn = document.getElementById("btn-slideShow");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+
 startBtn.addEventListener("click", (e) => {
 	console.log("Button has been clicked");
 	/* Text svg1 show after 1s */
 	startBtn.classList.add("disabled");
-	document.getElementById("one").classList.remove("disabled");
-	document.getElementById("one").classList.add("enabled");
-	document.getElementById("one").classList.add("one");
+	one.classList.remove("disabled");
+	one.classList.add("one");
+	document.getElementById("text-pos-1").classList.toggle("hidden");
 	console.log("page passed by timer");
 	setTimeout(transition1, 5000);
   });
 function transition1() {
 	console.log("page passed by function 1");
-	document.getElementById("one").classList.remove("enabled");
-	document.getElementById("one").classList.add("disabled");
-	document.getElementById("two").classList.remove("disabled");
-	document.getElementById("two").classList.add("enabled");
-	document.getElementById("two").classList.add("two");
+	one.classList.remove("enabled");
+	one.classList.add("disabled");
+	two.classList.remove("disabled");
+	two.classList.add("enabled");
+	two.classList.add("two");
 	setTimeout(transition2, 5000);
-}
+} /*
 function transition2() {
 	console.log("page passed by function 2");
-	document.getElementById("two").classList.remove("enabled");
-	document.getElementById("two").classList.add("disabled");
-	document.getElementById("three").classList.remove("disabled");
-	document.getElementById("three").classList.add("enabled");
-	document.getElementById("three").classList.add("three");
+	two.classList.remove("enabled");
+	two.classList.add("disabled");
+	three.classList.remove("disabled");
+	three.classList.add("enabled");
+	three.classList.add("three");
 	setTimeout(transition3, 5000);
 }
 function transition3() {
 	console.log("page passed by function 3");
-	document.getElementById("three").classList.remove("enabled");
-	document.getElementById("three").classList.add("disabled");
-	document.getElementById("four").classList.remove("disabled");
-	document.getElementById("four").classList.add("enabled");
-	document.getElementById("four").classList.add("four");
+	three.classList.remove("enabled");
+	three.classList.add("disabled");
+	four.classList.remove("disabled");
+	four.classList.add("enabled");
+	four.classList.add("four");
 	setTimeout(backToStart, 5000);
 }
 function backToStart() {
 	console.log("page passed by function 4");
-	document.getElementById("four").classList.remove("enabled");
-	document.getElementById("four").classList.add("disabled");
+	four.classList.remove("enabled");
+	four.classList.add("disabled");
 	startBtn.classList.remove("disabled");
 	startBtn.classList.add("enabled");
 }
 
+*/
